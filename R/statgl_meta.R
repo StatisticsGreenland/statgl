3e74b35d20fc575dbc84ed52d278c16b86739da4
Emil Malta <emilmaltamuller@gmail.com>
2023-08-04 23:48:21 +0200

    statgl_meta now returns tibble, gets rid of print

--- R/statgl_meta.R
+++ R/statgl_meta.R
@@ -8,49 +8,15 @@
 #' @export
 #'
 #' @examples
-#' statgl_meta(statgl_url("BEXST1"))
-statgl_meta <- function(url) {
+#' statgl_meta(statgl_url("BEXSTA"))
+statgl_meta <- function(url, ...) {
 
   if (!is_valid_url(url)) {
-    url <- toupper(url)
-    lang <- get_language_code(substr(url, 3, 3))
-    if (is.na(lang)) {
-      lang <- "en"
-    }
-    url <- paste0(substr(url, 1, 2), "X", substr(url, 4, nchar(url)))
-    url <- statgl_url(url, lang = lang)
+    url <- statgl_url(url)
   }
   api_get <- httr::content(httr::GET(url))
 
-  validate_px(url)
-
-  structure(api_get, class = "statgl_meta")
-}
-
-# Print function ---------------------------------------------------------------
-
-#' @export
-#' @importFrom utils head
-print.statgl_meta <- function(x, n_values = 6, ...) {
-
-  px_title <- x[["title"]]
-  px_vars <- x[["variables"]]
-
-  cat(paste("\nTitle:", px_title, "\n\n"))
-
-  for(i in px_vars) {
-
-    cat(paste0(
-      "  code: ", i[["code"]], "\n",
-      "  text: ", i[["text"]], "\n")
-    )
-
-    ellipses <- ifelse(length(i[["values"]]) > n_values, "...", "")
-    cat(
-      "  vals:",
-      paste0("{", head(i[["values"]], n_values), ":",
-             head(i[["valueTexts"]], n_values), "}"),
-      ellipses,
-      "\n\n")
-  }
+  cat(paste0(unique(api_get$title), "\n\n"))
+  meta_list <- lapply(api_get$variables, tibble::as_tibble)
+  lapply(meta_list, dplyr::mutate, values = unlist(values), valueTexts = unlist(valueTexts))
 }
