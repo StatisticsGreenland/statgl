724382860936f60f94eb808b5519b36a90b26107
Emil Malta <emilmaltamuller@gmail.com>
2025-05-05 09:12:54 +0200

    Added replace_0s to statgl_table

--- R/statgl_table.R
+++ R/statgl_table.R
@@ -11,11 +11,17 @@
 #'
 #' @examples
 #' statgl_table(ggplot2::mpg, year_col = year)
-statgl_table <- function(df, year_col, ...) {
+statgl_table <- function(df, year_col, replace_0s = FALSE, ...) {
   aligns <- paste0(c("l", rep("r", ncol(df))), collapse = "")
 
   if(!missing(year_col)){
     df <- dplyr::mutate(df, dplyr::across({{year_col}}, as.character))
+  }
+
+  if(replace_0s){
+    df <- dplyr::mutate_all(df, as.character)
+    df <- dplyr::mutate_all(df, trimws)
+    df <- dplyr::mutate_all(df, plyr::mapvalues, from = "0", to = "[-]{}", warn_missing = FALSE)
   }
 
   df <- dplyr::mutate_if(df, is.numeric, format, big.mark = ".", decimal.mark = ",", justify = "right", digits = 3, ...)
