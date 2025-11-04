b0eb98247fbf029fc632e16254716d2b5a238bf9
Emil Malta <emilmaltamuller@gmail.com>
2025-11-04 09:26:36 +0100

    Character table in statgl_table

--- R/statgl_table.R
+++ R/statgl_table.R
@@ -14,21 +14,35 @@
 statgl_table <- function(df, year_col, replace_0s = FALSE, ...) {
   aligns <- paste0(c("l", rep("r", ncol(df))), collapse = "")
 
-  if(!missing(year_col)){
-    df <- dplyr::mutate(df, dplyr::across({{year_col}}, as.character))
+  if (!missing(year_col)) {
+    df <- dplyr::mutate(df, dplyr::across({{ year_col }}, as.character))
   }
 
-  if(replace_0s){
+  if (replace_0s) {
     df <- dplyr::mutate_all(df, as.character)
     df <- dplyr::mutate_all(df, trimws)
-    df <- dplyr::mutate_all(df, plyr::mapvalues, from = "0", to = "[-]{}", warn_missing = FALSE)
+    df <- dplyr::mutate_all(
+      df,
+      plyr::mapvalues,
+      from = "0",
+      to = "[-]{}",
+      warn_missing = FALSE
+    )
   }
 
-  df <- dplyr::mutate_if(df, is.numeric, format, big.mark = ".", decimal.mark = ",", justify = "right", digits = 3, ...)
-
-  kableExtra::kable_styling(
-    kableExtra::kable(df, align = aligns),
-    bootstrap_options = c("striped", "hover", "condensed", "responsive")
+  df <- dplyr::mutate_if(
+    df,
+    is.numeric,
+    format,
+    big.mark = ".",
+    decimal.mark = ",",
+    justify = "right",
+    digits = 3,
+    ...
   )
 
+  as.character(kableExtra::kable_styling(
+    kableExtra::kable(df, align = aligns),
+    bootstrap_options = c("striped", "hover", "condensed", "responsive")
+  ))
 }
