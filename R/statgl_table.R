a76b23eb03351c07c844160678a6d47338a170d5
Emil Malta <emilmaltamuller@gmail.com>
2025-11-04 10:04:35 +0100

    Ability to manually convert statgl table to raw html

--- R/statgl_table.R
+++ R/statgl_table.R
@@ -1,48 +1,41 @@
-#' Automatic statgl table
-#'
-#' Concvenience function to quickly write html tables. Returns a kable with automatic alignments and formats.
-#'
-#' @param df A data frame
-#' @param year_col Name of year column. Disregards formatiing at this num column.
-#' @param ... Parameters to \code{format}
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' statgl_table(ggplot2::mpg, year_col = year)
-statgl_table <- function(df, year_col, replace_0s = FALSE, ...) {
-  aligns <- paste0(c("l", rep("r", ncol(df))), collapse = "")
-
+statgl_table <- function(
+  df,
+  year_col,
+  replace_0s = FALSE,
+  ...,
+  digits = 3,
+  .as_html = FALSE
+) {
+  aligns <- paste0(c("l", rep("r", ncol(df) - 1L)), collapse = "")
   if (!missing(year_col)) {
     df <- dplyr::mutate(df, dplyr::across({{ year_col }}, as.character))
   }
-
   if (replace_0s) {
-    df <- dplyr::mutate_all(df, as.character)
-    df <- dplyr::mutate_all(df, trimws)
-    df <- dplyr::mutate_all(
-      df,
-      plyr::mapvalues,
-      from = "0",
-      to = "[-]{}",
-      warn_missing = FALSE
-    )
+    df[] <- lapply(df, function(x) {
+      x <- trimws(as.character(x))
+      x[x == "0"] <- "[-]{}"
+      x
+    })
   }
-
   df <- dplyr::mutate_if(
     df,
     is.numeric,
     format,
     big.mark = ".",
     decimal.mark = ",",
+    digits = digits,
     justify = "right",
-    digits = 3,
     ...
   )
 
-  kableExtra::kable_styling(
-    kableExtra::kable(df, align = aligns),
-    bootstrap_options = c("striped", "hover", "condensed", "responsive")
-  )
+  k <- df |>
+    kableExtra::kable(format = "html", align = aligns, escape = FALSE) |>
+    kableExtra::kable_styling(
+      bootstrap_options = c("striped", "hover", "condensed", "responsive")
+    )
+
+  if (.as_html) {
+    return(as.character(k))
+  }
+  k
 }
